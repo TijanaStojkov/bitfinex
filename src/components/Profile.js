5aa1cce8e62d0d6a4752631c5a102cd283afc982
Tijana Stojkov <stojkov.tijana.80@gmail.com>
2020-12-13 15:01:04 +0100

    cln

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 //materialize
 import { Row, Icon, Col, Card, CardTitle, Button } from 'react-materialize';
@@ -7,12 +7,12 @@ const img1 = 'https://api.hello-avatar.com/adorables/285';
 const img2 = 'https://api.hello-avatar.com/adorables/285/%3cYOUR_EMAIL';
 
 const Profile = () => {
-    const [img, setImg] = useState(img1)
+    const [img, setImg] = useState(img1);
     
     const toggleImage = () => {
         const currentImage = img;
         const newImage = currentImage===img1?img2:img1;
-        setImg(newImage)
+        setImg(newImage);
     }
 
     return (
@@ -24,15 +24,15 @@ const Profile = () => {
                     s={12}
                 >
                     <Card
-                    actions={[
-                        <Button key="1" onClick={toggleImage} style={{backgroundColor:'#F0DA5E', color:'#000', fontSize:'15px'}}>
-                            Toggle avatar
-                        </Button>
-                    ]}
-                    closeIcon={<Icon>close</Icon>}
-                    header={<CardTitle image={img}></CardTitle>}
-                    revealIcon={<Icon>more_vert</Icon>}
-                    >
+                        actions={[
+                            <Button key="1" onClick={toggleImage} style={{backgroundColor:'#F0DA5E', color:'#000', fontSize:'15px'}}>
+                                Toggle avatar
+                            </Button>
+                        ]}
+                        closeIcon={<Icon>close</Icon>}
+                        header={<CardTitle image={img}></CardTitle>}
+                        revealIcon={<Icon>more_vert</Icon>}
+                        >
                         <h5>Goran Urukalo</h5>
                         <p>goran.urukalo@teletrader.com</p>
                         <p>http://goran.urukalo.github.io</p>
@@ -41,6 +41,5 @@ const Profile = () => {
             </Row>
         </div>
     )
-
 }
 export default Profile;
--- src/spinner/Spinner.js
+++ src/spinner/Spinner.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './Spinner.css'
+import './Spinner.css';
 
 const spinner = () => (
     <div style={{width:'14px', height:'14px'}}>
-    <div className="loader">Loading...</div>
-
+        <div className="loader">Loading...</div>
     </div>
 )
 export default spinner;
